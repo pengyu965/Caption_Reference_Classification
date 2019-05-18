62d1f00e43aa731c5873c9a89687ea94459bfbf0
pengyu965 <py.yan965@gmail.com>
2019-05-18 12:19:28 -0400

    update torch model

--- model_torch/cnn_model.py
+++ model_torch/cnn_model.py
@@ -6,8 +6,8 @@ import torch.optim as optim
 from torch.optim import lr_scheduler
 
 import time 
-
 import json 
+import fastText
 
 
 class Model(nn.Module):
@@ -42,7 +42,68 @@ class Model(nn.Module):
         return x
 
 class Trainer:
+    def __init__(self, input_dir, model, epoch = 10, class_num = 3):
+        self.input_dir = input_dir 
+        self.model = model 
+        self.epoch = epoch
+        self.class_num = class_num
+
+        self.lr = self.model.lr
+        self.batch_size = self.model.batch_size
+        
+
+        self.loss = self.model.loss()
+        self.accuracy = self.model.accuracy()
+        self.optimizer = self.model.optimizer()
+
+        self.caption_train = json.load(open(self.input_dir+"train_caption_json.json", 'r'))[:-300]
+        self.caption_val = json.load(open(self.input_dir+"train_caption_json.json", 'r'))[-300:]
+        self.reference_train = json.load(open(self.input_dir+"train_reference_json.json", 'r'))[:-300]
+        self.reference_val = json.load(open(self.input_dir+"train_reference_json.json", 'r'))[-300:]
+        self.negative_train = json.load(open(self.input_dir+"train_negative_json.json", 'r'))[:-300]
+        self.negative_val = json.load(open(self.input_dir+"train_negative_json.json", 'r'))[-300:]
+
+        # self.word2vec_model = word2vec.load('./embedding/word2vec.bin')
+        self.word2vec_model = fastText.FastText.load_model('./embedding/fil9.bin')
     
 
 
 
+
+
+def sentence_embedding(dic_list, word2vec_model, category):
+    batch_list = []
+    for dic in dic_list:
+        token_list = nltk.word_tokenize(dic["Text"])
+        
+        sample = []
+
+        sent_matrix = []
+        pos_matrix = []
+
+        
+        for token in token_list:
+            # word_vec = word2vec_model[token].tolist()
+            word_vec = word2vec_model.get_word_vector(token).tolist()
+            
+            sent_matrix.append(word_vec)
+            
+            if token == ",":
+                pos_matrix.append([1]*50+[0]*50)
+            elif token == ".":
+                pos_matrix.append([0]*50+[1]*50)
+            else:
+                # pos_matrix.append(word2vec_model[str(position)])
+                pos_matrix.append(word2vec_model.get_word_vector(token))
+        
+        sent_matrix += [[0]*100]*(200-len(sent_matrix))
+        pos_matrix += [[0]*100]*(200-len(pos_matrix))
+        sent_matrix = np.expand_dims(np.array(sent_matrix), axis = -1)
+        pos_matrix = np.expand_dims(np.array(pos_matrix), axis = -1)
+        concated_matrix = np.concatenate((sent_matrix, pos_matrix), axis = -1).tolist()
+
+        sample.append([concated_matrix, category])
+
+        batch_list.append(sample)
+    
+    return batch_list
